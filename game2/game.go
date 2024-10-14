74a84d838855ef12fb28781e16fbc88ae35c8ad1
owen <owen@gmail.com>
2024-10-14 14:39:49 +0800

    update

--- game2/game.go
+++ game2/game.go
@@ -5,50 +5,38 @@ import (
 	"bitbucket.org/alibaba-international/go-pkg/errorx"
 )
 
-type gameName string
-type gameCode string
+type GameName string
+type GameCode string
 
-type Game interface {
-	GetGameCode() gameCode
+type GameObject interface {
+	GetGameName() *GameName
+	GetGameCode() *GameCode
 }
 
-type codes map[model.Environment]map[gameCode]gameName
+type Codes[T GameObject] map[model.Environment]map[GameCode]T
 
-func (c *codes) GetGameName(env model.Environment, code gameCode) (*gameName, errorx.Error) {
+func (c *Codes[T]) GetGameName(env model.Environment, code GameCode) (*T, errorx.Error) {
 	m, ok := (*c)[env]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	name, ok := m[code]
+	obj, ok := m[code]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	return &name, nil
+	return &obj, nil
 }
 
-type games[T Game] map[model.Environment]map[gameName]T
+type Games[T GameObject] map[model.Environment]map[GameName]T
 
-func (c *games[T]) GetGame(env model.Environment, game gameName) (*T, errorx.Error) {
+func (c *Games[T]) GetGame(env model.Environment, game GameName) (*T, errorx.Error) {
 	m, ok := (*c)[env]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	code, ok := m[game]
+	obj, ok := m[game]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	return &code, nil
-}
-
-func (c *games[T]) GetGameCode(env model.Environment, game gameName) (*gameCode, errorx.Error) {
-	m, ok := (*c)[env]
-	if !ok {
-		return nil, errorx.New("game not found")
-	}
-	code, ok := m[game]
-	if !ok {
-		return nil, errorx.New("game not found")
-	}
-	code1 := code.GetGameCode()
-	return &code1, nil
+	return &obj, nil
 }

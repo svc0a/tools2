a89f9d19fd0522291ed0d20628d4602922b0f750
owen <owen@gmail.com>
2024-10-14 09:38:42 +0800

    update

--- adapter2/server.go
+++ adapter2/server.go
@@ -2,32 +2,34 @@ package adapter2
 
 import (
 	"bitbucket.org/alibaba-international/ghs-common/adapter"
+	"bitbucket.org/alibaba-international/ghs-common/model"
 	"bitbucket.org/alibaba-international/go-pkg/ctxx"
 	"bitbucket.org/alibaba-international/go-pkg/errorx"
 )
 
-func NewServer(providerName string, gameGameLink getGameLinkFunc, checkOrderState checkOrderStateFunc) adapter.Server {
-	server1 := adapter.NewServer(providerName, map[adapter.Environment]adapter.Client{
-		adapter.EnvTest: newClient(adapter.EnvTest, gameGameLink, checkOrderState),
-		adapter.EnvProd: newClient(adapter.EnvProd, gameGameLink, checkOrderState),
-	})
-	return server1
+type Servers map[model.Environment]adapter.Server
+
+func NewServer(providerName string, gameGameLink getGameLinkFunc, stateFunc getBetStateByIDFunc) Servers {
+	return map[model.Environment]adapter.Server{
+		model.EnvTest: adapter.NewServer(model.EnvTest, providerName, newClient(model.EnvTest, gameGameLink, stateFunc)),
+		model.EnvProd: adapter.NewServer(model.EnvProd, providerName, newClient(model.EnvProd, gameGameLink, stateFunc)),
+	}
 }
 
-type getGameLinkFunc func(ctx ctxx.Context, cmd adapter.EnterGameCommand, env adapter.Environment) (*adapter.EnterGameResult, errorx.Error)
-type checkOrderStateFunc func(ctx ctxx.Context, cmd adapter.CheckOrderStateCommand, env adapter.Environment) (*adapter.CheckOrderStateResult, errorx.Error)
+type getGameLinkFunc func(ctx ctxx.Context, cmd adapter.EnterGameCommand, env model.Environment) (*adapter.EnterGameResult, errorx.Error)
+type getBetStateByIDFunc func(ctx ctxx.Context, betID string, env model.Environment) (*adapter.BetState, errorx.Error)
 
 type client struct {
-	env             adapter.Environment
-	getGameLink     getGameLinkFunc
-	checkOrderState checkOrderStateFunc
+	env                 model.Environment
+	getGameLink         getGameLinkFunc
+	getBetStateByIDFunc getBetStateByIDFunc
 }
 
-func newClient(env adapter.Environment, linkFunc getGameLinkFunc, stateFunc checkOrderStateFunc) *client {
+func newClient(env model.Environment, linkFunc getGameLinkFunc, stateFunc getBetStateByIDFunc) *client {
 	return &client{
-		env:             env,
-		getGameLink:     linkFunc,
-		checkOrderState: stateFunc,
+		env:                 env,
+		getGameLink:         linkFunc,
+		getBetStateByIDFunc: stateFunc,
 	}
 }
 
@@ -35,6 +37,6 @@ func (c *client) GetGameLink(ctx ctxx.Context, cmd adapter.EnterGameCommand) (*a
 	return c.getGameLink(ctx, cmd, c.env)
 }
 
-func (c *client) CheckOrderState(ctx ctxx.Context, cmd adapter.CheckOrderStateCommand) (*adapter.CheckOrderStateResult, errorx.Error) {
-	return c.checkOrderState(ctx, cmd, c.env)
+func (c *client) GetBetStateByID(ctx ctxx.Context, betID string) (*adapter.BetState, errorx.Error) {
+	return c.getBetStateByIDFunc(ctx, betID, c.env)
 }

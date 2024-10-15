19229556cee4f19ce00036b38bf3752bf800e6a6
owen <owen@gmail.com>
2024-10-15 10:15:41 +0800

    update

--- game2/game.go
+++ game2/game.go
@@ -3,40 +3,69 @@ package game2
 import (
 	"bitbucket.org/alibaba-international/ghs-common/model"
 	"bitbucket.org/alibaba-international/go-pkg/errorx"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
 )
 
 type GameName string
 type GameCode string
 
 type GameObject interface {
-	GetGameName() *GameName
-	GetGameCode() *GameCode
+	GetName() *GameName
+	GetCode() *GameCode
 }
 
-type Codes[T GameObject] map[model.Environment]map[GameCode]T
-
-func (c Codes[T]) GetGameName(env model.Environment, code GameCode) (*T, errorx.Error) {
-	m, ok := c[env]
-	if !ok {
-		return nil, errorx.New("game not found")
-	}
-	obj, ok := m[code]
-	if !ok {
-		return nil, errorx.New("game not found")
-	}
-	return &obj, nil
+type GameHelper[T GameObject] interface {
+	GetByName(name GameName) (*T, errorx.Error)
+	GetByCode(code GameCode) (*T, errorx.Error)
 }
 
-type Games[T GameObject] map[model.Environment]map[GameName]T
+type gameHelper[T GameObject] struct {
+	codes map[GameCode]T
+	games map[GameName]T
+}
 
-func (c Games[T]) GetGame(env model.Environment, game GameName) (*T, errorx.Error) {
-	m, ok := c[env]
+type Helper[T GameObject] map[model.Environment]GameHelper[T]
+
+func (h Helper[T]) Get(env model.Environment) GameHelper[T] {
+	return h[env]
+}
+
+func NewHelpers[T GameObject](in string) Helper[T] {
+	m2 := map[model.Environment][]T{}
+	err := json.Unmarshal([]byte(in), &m2)
+	if err != nil {
+		logrus.Fatal(err)
+		return nil
+	}
+	m := Helper[T]{
+		model.EnvTest: newHelper(m2[model.EnvTest]),
+		model.EnvProd: newHelper(m2[model.EnvProd]),
+	}
+	return m
+}
+
+func newHelper[T GameObject](in []T) GameHelper[T] {
+	g := gameHelper[T]{}
+	for _, item := range in {
+		g.codes[*item.GetCode()] = item
+		g.games[*item.GetName()] = item
+	}
+	return &g
+}
+
+func (g *gameHelper[T]) GetByName(name GameName) (*T, errorx.Error) {
+	r, ok := g.games[name]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	obj, ok := m[game]
+	return &r, nil
+}
+
+func (g *gameHelper[T]) GetByCode(code GameCode) (*T, errorx.Error) {
+	r, ok := g.codes[code]
 	if !ok {
 		return nil, errorx.New("game not found")
 	}
-	return &obj, nil
+	return &r, nil
 }

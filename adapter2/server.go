eaa8949588c3c6bda2be8cda86e814e024123a46
owen <owen@gmail.com>
2024-10-15 14:12:23 +0800

    update

--- adapter2/server.go
+++ adapter2/server.go
@@ -12,27 +12,24 @@ type Servers map[model.Environment]adapter.Server
 func NewServer(providerName string, gameGameLink getGameLinkFunc, stateFunc getBetStateByIDFunc) Servers {
 	testClient := newClient(model.EnvTest, gameGameLink, stateFunc)
 	testServer := adapter.NewServer(model.EnvTest, providerName, testClient)
-	testClient.server = testServer
-	testServer = adapter.NewServer(model.EnvTest, providerName, testClient)
-
+	testClient.Server = testServer
 	prodClient := newClient(model.EnvProd, gameGameLink, stateFunc)
-	prodServer := adapter.NewServer(model.EnvTest, providerName, prodClient)
-	prodClient.server = prodServer
-	prodServer = adapter.NewServer(model.EnvProd, providerName, prodClient)
+	prodServer := adapter.NewServer(model.EnvProd, providerName, prodClient)
+	prodClient.Server = prodServer
 	return map[model.Environment]adapter.Server{
 		model.EnvTest: testServer,
 		model.EnvProd: prodServer,
 	}
 }
 
-type getGameLinkFunc func(ctx ctxx.Context, cmd adapter.EnterGameCommand, env model.Environment) (*adapter.EnterGameResult, errorx.Error)
+type getGameLinkFunc func(ctx ctxx.Context, cmd adapter.EnterGameCommand, env model.Environment, server adapter.Server) (*adapter.EnterGameResult, errorx.Error)
 type getBetStateByIDFunc func(ctx ctxx.Context, betID string, env model.Environment) (*adapter.BetState, errorx.Error)
 
 type client struct {
 	env                 model.Environment
 	getGameLink         getGameLinkFunc
 	getBetStateByIDFunc getBetStateByIDFunc
-	server              adapter.Server
+	adapter.Server
 }
 
 func newClient(env model.Environment, linkFunc getGameLinkFunc, stateFunc getBetStateByIDFunc) *client {
@@ -44,7 +41,7 @@ func newClient(env model.Environment, linkFunc getGameLinkFunc, stateFunc getBet
 }
 
 func (c *client) GetGameLink(ctx ctxx.Context, cmd adapter.EnterGameCommand) (*adapter.EnterGameResult, errorx.Error) {
-	return c.getGameLink(ctx, cmd, c.env)
+	return c.getGameLink(ctx, cmd, c.env, c.Server)
 }
 
 func (c *client) GetBetStateByID(ctx ctxx.Context, betID string) (*adapter.BetState, errorx.Error) {
